ed613bc68d13caabe65cbd0c9c55fc1a80d0b989
Henry Scheible <henry.scheible@gmail.com>
2022-10-07 09:44:51 -0400

    Update stereoset.py

--- nlpcore/bias_datasets/stereoset.py
+++ nlpcore/bias_datasets/stereoset.py
@@ -5,45 +5,58 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, Data
 import numpy as np
 import evaluate
 
+from nlpcore.bert.bert import load_bert_model
+
 
 def load_stereoset():
-    return load_dataset("crows_pairs")['test']
+    return load_dataset("stereoset", "intersentence")['validation']
 
 
-def process_steroset(dataset, tokenizer, batch_size=64):
-    def add_label(example):
-        if example["label"] == 1:
-            return {
-                "sent_a": example["sent_more"],
-                "sent_b": example["sent_less"]
-            }
-        else:
-            return {
-                "sent_a": example["sent_less"],
-                "sent_b": example["sent_more"]
-            }
+def process_fn_binary(example):
+    sentences = []
+    labels = []
+    contexts = []
+    for i in range(3):
+        if example["sentences"][0]["gold_label"][i] != 2:
+            sentences.append(example["sentences"][0]["sentence"][i])
+            labels.append(example["sentences"][0]["gold_label"][i])
+            contexts.append(example["context"][0])
+    return {
+        "sentence": sentences,
+        "label": labels,
+        "context": contexts
+    }
+
+def process_fn_all(example):
+    sentences = []
+    labels = []
+    contexts = []
+    for i in range(3):
+        sentences.append(example["sentences"][0]["sentence"][i])
+        labels.append(example["sentences"][0]["gold_label"][i])
+        contexts.append(example["context"][0])
+    return {
+        "sentence": sentences,
+        "label": labels,
+        "context": contexts
+    }
+
+
+
+def process_stereoset(dataset, tokenizer, batch_size=64, include_unrelated=False):
 
     def tokenize(example):
-        return tokenizer(example["sent_a"], example["sent_b"], truncation=True, padding=True)
+        return tokenizer(example["context"], example["sentence"], truncation=True, padding=True)
 
-    num_samples = len(dataset["sent_more"])
+    num_samples = len(dataset["id"])
     dataset = dataset.remove_columns([
-        "stereo_antistereo",
-        "bias_type",
-        "annotations",
-        "anon_writer",
-        "anon_annotators",
-    ])
-    dataset = dataset.add_column("label", np.random.choice(2, num_samples))
-    dataset_processed = dataset.map(add_label, batched=False)
-    tokenized_dataset = dataset_processed.map(tokenize, batched=True, batch_size=64)
-    tokenized_dataset = tokenized_dataset.remove_columns([
         "id",
-        "sent_more",
-        "sent_less",
-        "sent_a",
-        "sent_b"
+        "target",
+        "bias_type",
     ])
+    process_fn = process_fn_all if include_unrelated else process_fn_binary
+    dataset_processed = dataset.map(process_fn, batched=True, batch_size=1, remove_columns=["sentences"])
+    tokenized_dataset = dataset_processed.map(tokenize, batched=True, batch_size=64, remove_columns=["context", "sentence"])
     split_tokenized_dataset = tokenized_dataset.train_test_split(
         test_size=0.2
     )
@@ -52,5 +65,3 @@ def process_steroset(dataset, tokenizer, batch_size=64):
     eval_dataloader = DataLoader(split_tokenized_dataset["test"], batch_size=batch_size, collate_fn=data_collator)
 
     return train_dataloader, eval_dataloader
-
-
